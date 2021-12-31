dc2751f58f0174cb3deade3b0572d09b09a4fce5
liao2000 <liao2000@livemail.tw>
2021-12-31 14:21:31 +0800

    Update "project" and add "notebooks"

--- web/main.dart
+++ web/main.dart
@@ -1,27 +1,31 @@
 import 'dart:html';
+import 'dart:math';
 
 var page = {
   'main': 'About me',
   'projects': 'Projects',
+  'notebooks': 'Notebooks',
   'resume': 'Résumé'
 };
 
-void routingPage(String r, {bool push = true}){
+void routingPage(String r, {bool push = true}) {
   querySelector('#routing-loader')?.style.display = 'block';
-  page.forEach((k, v){
-    querySelector('#page-$k')?.style.display = (r != k)? 'none' : 'block';
-    querySelector('#routing-$k')?.className = (r != k)? '' : 'active';
+  page.forEach((k, v) {
+    querySelector('#page-$k')?.style.display = (r != k) ? 'none' : 'block';
+    querySelector('#routing-$k')?.className = (r != k) ? '' : 'active';
   });
   querySelector('#routing-loader')?.style.display = 'none';
-  if(push){
-    window.history.pushState({'title': page[r]}, page[r] ?? '', (r=='main')?'':'#$r');
-  }else{
-    window.history.replaceState({'title': page[r]}, page[r] ?? '', (r=='main')?'':'#$r');
+  if (push) {
+    window.history.pushState(
+        {'title': page[r]}, page[r] ?? '', (r == 'main') ? '' : '#$r');
+  } else {
+    window.history.replaceState(
+        {'title': page[r]}, page[r] ?? '', (r == 'main') ? '' : '#$r');
   }
   document.title = '${page[r]} :: Bocheng Liao';
 }
 
-void autoRoute(){
+void autoRoute() {
   var exp = RegExp(r'(.*?)#(.*)');
   Match? m = exp.firstMatch(window.location.href);
   routingPage((m == null) ? 'main' : m[2]!, push: false);
@@ -29,14 +33,23 @@ void autoRoute(){
 
 void main() {
   autoRoute();
-  page.forEach((k,_) {
-    querySelector('#routing-$k a')?.addEventListener('click', (event){
+  page.forEach((k, _) {
+    querySelector('#routing-$k a')?.addEventListener('click', (event) {
       routingPage(k);
     });
   });
 
-  window.addEventListener('popstate', (event){
+  window.addEventListener('popstate', (event) {
     autoRoute();
   });
-}
 
+  querySelectorAll('img[data-repo]').forEach((dom) {
+    var rng = Random();
+    var repo = dom.getAttribute('data-repo');
+    var url = Uri.parse(
+            'https://opengraph.githubassets.com/${rng.nextInt(100000)}/$repo')
+        .toString();
+    print(url);
+    dom.setAttribute('src', url);
+  });
+}
