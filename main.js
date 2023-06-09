355efa6c422ddaeea71840c250e8e2a63d95da05
Kashiwa <liao2000@livemail.tw>
2023-06-09 10:58:00 +0800

    Use javascript instead of dart

---
+++ main.js
@@ -0,0 +1,77 @@
+const page = {
+  'main': 'About me',
+  'projects': 'Projects',
+  'notebooks': 'Notebooks',
+  'resume': 'Résumé',
+  'links': 'Links',
+};
+
+function routingPage(r, push) {
+  // if(typeof push === 'undefined') push = true;
+  // push = push ?? true;
+  const routingLoader = document.querySelector('#routing-loader');
+  if (routingLoader) {
+    routingLoader.style.display = 'block';
+  }
+  
+  Object.keys(page).forEach((k) => {
+    const pageElement = document.querySelector(`#page-${k}`);
+    const routingElement = document.querySelector(`#routing-${k}`);
+    
+    if (pageElement) {
+      pageElement.style.display = (r !== k) ? 'none' : 'block';
+    }
+    
+    if (routingElement) {
+      routingElement.className = (r !== k) ? '' : 'active';
+    }
+  });
+  
+  if (routingLoader) {
+    routingLoader.style.display = 'none';
+  }
+  
+  if (push) {
+    const title = page[r] ? {'title': page[r]} : {};
+    const url = (r === 'main') ? '' : `#${r}`;
+    window.history.pushState(title, page[r], url);
+  } else {
+    const title = page[r] ? {'title': page[r]} : {};
+    const url = (r === 'main') ? '' : `#${r}`;
+    window.history.replaceState(title, page[r], url);
+  }
+  
+  document.title = `${page[r]} :: Bocheng Liao`;
+}
+
+function autoRoute() {
+  const exp = /(.*?)#(.*)/;
+  const m = exp.exec(window.location.href);
+  routingPage(m === null ? 'main' : m[2], false);
+}
+
+function main() {
+  autoRoute();
+  
+  Object.keys(page).forEach((k) => {
+    const routingElement = document.querySelector(`#routing-${k} a`);
+    if (routingElement) {
+      routingElement.addEventListener('click', () => {
+        routingPage(k, true);
+      });
+    }
+  });
+  
+  window.addEventListener('popstate', () => {
+    autoRoute();
+  });
+  
+  document.querySelectorAll('img[data-repo]').forEach((dom) => {
+    const rng = Math.floor(Math.random() * 100000);
+    const repo = dom.getAttribute('data-repo');
+    const url = `https://opengraph.githubassets.com/${rng}/${repo}`;
+    dom.setAttribute('src', url);
+  });
+}
+
+main();
